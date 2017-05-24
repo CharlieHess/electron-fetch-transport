e5c600b465f467817abdbd0e4565c36252ba76a8
Charlie Hess <charlie@slack-corp.com>
2017-05-23 23:56:55 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # electron-fetch-transport
-A transport for https://github.com/slackapi/node-slack-sdk that uses Electron's net module
+
+A transport for [`node-slack-sdk`](https://github.com/slackapi/node-slack-sdk) that uses Electron's [`net`](https://electron.atom.io/docs/api/net/) module.
+This allows you to use the the web & rtm clients from Electron's main process and still reap the benefits of Chromium's networking stack.
+
+## Installation
+
+```
+npm i electron-fetch-transport --save
+```
+
+## Usage
+
+``` js
+import { RtmClient } from '@slack/client';
+import netTransport from 'electron-fetch-transport';
+
+// Use Electron's `net` module for requests
+const rtm = new RtmClient(token, {
+  transport: netTransport,
+  ...
+});
+```
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import fetch from 'electron-fetch'
+import FormData from 'form-data'
+
+/**
+ * Implements a transport for {@link https://github.com/slackapi/node-slack-sdk}
+ * that using `electron-fetch`, which under the surface uses the `net` module.
+ *
+ * @export
+ * @param {Object} args       Request arguments, includes URL, data, & headers
+ * @param {Function} callback A callback to invoke with parameters error,
+ *                            headers, status, and body
+ */
+export default async function transport(args, callback) {
+  try {
+    const response = await fetch(args.url, {
+      method: 'POST',
+      body: formDataFromArgs(args.data),
+      headers: args.headers
+    });
+
+    const body = await response.text();
+    callback(null, response.headers, response.status, body);
+  } catch (error) {
+    callback(error, {}, null, null);
+  }
+};
+
+function formDataFromArgs(data) {
+  return Object.keys(data).reduce((form, key) => {
+    form.append(key, data[key]);
+    return form;
+  }, new FormData())
+}
